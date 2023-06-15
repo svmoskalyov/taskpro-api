4970c5ef3916c0974a494c2a8464b975fcf62047
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-06-15 16:20:31 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const { DB_HOST, PORT = 5000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
