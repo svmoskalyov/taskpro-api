59d2226376f255a3679ce3d6efd4366e42e8bc98
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-06-19 18:55:22 +0300

    minifix2

--- middlewares/google-authenticate.js
+++ middlewares/google-authenticate.js
@@ -5,12 +5,12 @@ const { v4: uuidv4 } = require("uuid");
 
 const { User } = require("../models/user");
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL } = process.env;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BACKEND_URL } = process.env;
 
 const googleParams = {
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: `${BASE_URL}/users/google/callback`,
+  callbackURL: `${BACKEND_URL}/users/google/callback`,
   passReqToCallback: true,
 };
 
